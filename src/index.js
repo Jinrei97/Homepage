6ea4946af3c6f2f49635f373717c6578632a32a8
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-09-25 16:30:08 +0200

    header changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./index.css";
 import header_img from "./img/header_img.jpg";
 import footer_img from "./img/footer_img.jpg";
+import project_placeholder from "./img/project_placeholder.jpg";
 
 document.querySelector(".header_img").src = header_img;
 document.querySelector(".footer_img").src = footer_img;
+document.querySelectorAll(".card > img").forEach((img) => {
+  img.src = project_placeholder;
+});
