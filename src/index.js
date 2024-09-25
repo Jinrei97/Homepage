7680c1013776cd536290bfc6050ae2e1805ee088
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-09-25 14:13:00 +0200

    Added header and footer images.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import * as css from "./index.css";
-console.log("linked");
+import "./index.css";
+import header_img from "./img/header_img.jpg";
+import footer_img from "./img/footer_img.jpg";
+
+document.querySelector("header img").src = header_img;
+document.querySelector("footer img").src = footer_img;
