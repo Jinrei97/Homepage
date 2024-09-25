5aad28278c4ffcc16dc1cd358291549ec293230a
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-09-25 16:03:55 +0200

    Header.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import "./index.css";
 import header_img from "./img/header_img.jpg";
 import footer_img from "./img/footer_img.jpg";
 
-document.querySelector("header img").src = header_img;
-document.querySelector("footer img").src = footer_img;
+document.querySelector(".header_img").src = header_img;
+document.querySelector(".footer_img").src = footer_img;
